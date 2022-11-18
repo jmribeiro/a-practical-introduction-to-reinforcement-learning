07651b70576e25c3c369a464834f8c3b813fc4db
jmribeiro <jmribeiro77209@gmail.com>
2022-11-18 20:27:16 +0000

    Added pre-trained Actor-Critic to run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -6,6 +6,7 @@ import numpy as np
 
 import gym
 import torch
+from tqdm import tqdm
 
 from agents import ActorCritic
 from utils_execution import run_episode
@@ -16,9 +17,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--environment",            type=str,   default="LunarLander-v2")
-    parser.add_argument("--num-training-episodes",  type=int,   default=1000)
+    parser.add_argument("--num-training-episodes",  type=int,   default=3000)
     parser.add_argument("--num-demo-episodes",      type=int,   default=3)
     parser.add_argument("--seed",                   type=int,   default=42)
+    parser.add_argument("--save-dir",               type=str,   default="resources/pretrained_agents/ac_demo")
 
     opt = parser.parse_args()
 
@@ -44,19 +46,27 @@ if __name__ == '__main__':
         reward = run_episode(agent, env, max_timesteps=math.inf, render=True)
         print(f"Demo Episode {episode+1}/{opt.num_demo_episodes}: Reward={round(reward, 3)}")
 
-    print(f"\n\nTraining for {opt.num_training_episodes} episodes")
-    agent.train()
-    rewards = []
-    for episode in range(opt.num_training_episodes):
-        reward = run_episode(agent, env, max_timesteps=math.inf, render=False)
-        rewards.append(reward)
-        print(f"Training Episode {episode+1}/{opt.num_training_episodes}: Reward = {round(reward, 3)}", end="\r")
+    try:
 
-    print(f"\nPlotting Learning Curves to Plot-Demo.png")
-    plot_learning_curves("Demo", "MountainCar-v0", {"ActorCritic": {opt.seed: np.array(rewards)}})
+        agent.network.load(f"{opt.save_dir}_{opt.num_training_episodes}eps")
+        print(f"\n\nLoaded pre-trained agent")
+
+    except FileNotFoundError:
+
+        print(f"\n\nTraining for {opt.num_training_episodes} episodes")
+        agent.train()
+        rewards = []
+        for episode in tqdm(range(opt.num_training_episodes)):
+            reward = run_episode(agent, env, max_timesteps=math.inf, render=False)
+            rewards.append(reward)
+        print(f"\nPlotting Learning Curves to Plot-Demo.png")
+        plot_learning_curves("Demo", "MountainCar-v0", {"ActorCritic": {opt.seed: np.array(rewards)}})
+        agent.network.save(f"{opt.save_dir}_{opt.num_training_episodes}eps")
+
+    input("Enter any key to start the demo for trained agent")
 
     print(f"\n\nDemo for trained agent ({opt.num_demo_episodes} episodes)")
     agent.eval()
     for episode in range(opt.num_demo_episodes):
-        reward = run_episode(agent, env, max_timesteps=math.inf, render=True)
+        reward = run_episode(agent, env, max_timesteps=250, render=True)
         print(f"Demo Episode {episode + 1}/{opt.num_demo_episodes}: Reward={round(reward, 3)}")
